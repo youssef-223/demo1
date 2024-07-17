8af7fd289e77449f4fb799b5a8ba19f4f458a635
youssef-223 <109833289+youssef-223@users.noreply.github.com>
2024-07-17 14:35:36 +0300

    testing crontab

--- src/main/java/com/example/service/PersonService.java
+++ src/main/java/com/example/service/PersonService.java
@@ -40,6 +40,13 @@ public class PersonService {
 		p.setLastName("Mahmoud");
 		persons.add(p);
 		
+		p = new Person();
+		p.setId("5");
+		p.setAge(21);
+		p.setFirstName("crontab");
+		p.setLastName("test");
+		persons.add(p);
+		
 	}
 	public Person getPerson(String id) {
 		for(Person person:persons) {
